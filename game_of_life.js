11e63468f34394f6cff2532d996d470568c97754
dixebrau <45236048+dixebrau@users.noreply.github.com>
2018-11-27 23:30:09 +0100

    Updated appereance

--- game_of_life.js
+++ game_of_life.js
@@ -44,9 +44,39 @@ function clearBlock(i,j){
   ctx.fillRect(i*pw + 1, j*ph + 1, pw - 1, ph - 1);
 };
 
+
 // Logical functions
 
+function initCells(){
+  var cells = [];
+  for(var i = 0; i <= ny; i+=1){
+    cells[i] = [];
+    for(var j = 0; j <= nx; j+=1){
+      cells[i][j] = 0;
+    }
+  }
+};
+
+function clearBoard(){
+  // Clears both the cell state matrix and the graphics
+  // States
+  initCells();
+  // Graphics
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBoard();
+};
+
+function start(){
+  var niter = document.getElementById("generations").value;
+  var timestep = document.getElementById("timestep").value*1000;
+  for(var i = 0; i <= niter; i += 1){
+    (function(i) { setTimeout(function() {
+        document.getElementById("demo").innerHTML = "Generation " + i.toString();
+      }, timestep*i);
+    })(i);
+  };
+};
+
+
 drawBoard();
-fillBlock(nx/2,ny/2);
-fillBlock(nx/2+1,ny/2+1);
-clearBlock(nx/2+1,ny/2+1);
+fillBlock(nx/2, ny/2);
