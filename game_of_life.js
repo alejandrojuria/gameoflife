5c640622fb3125b6ee81f5a9740b1cd04baa6513
dixebrau <45236048+dixebrau@users.noreply.github.com>
2018-11-27 15:09:47 +0100

    Add files via upload
    First commit

---
+++ game_of_life.js
@@ -0,0 +1,54 @@
+// Game of life - Alejandro José Uría Álvarez
+
+// Variables
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var bh = 360;
+var bw = 640;
+
+var ph = 18;
+var pw = 16;
+
+var nx = bw/pw;
+var ny = bh/ph;
+
+canvas.height = bh;
+canvas.width = bw;
+canvas.style.border = "1px solid";
+
+// Graphical functions
+
+function drawBoard(){
+  // Vertical lines
+  for(var x = pw; x < bw; x+=pw){
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, bh);
+  }
+  // Horizontal lines
+  for(var y = ph; y < bh; y+=ph){
+    ctx.moveTo(0, y);
+    ctx.lineTo(bw, y);
+  }
+  ctx.strokeStyle = "black";
+  ctx.stroke();
+};
+
+function fillBlock(i,j){
+  ctx.fillStyle="#000000";
+  ctx.fillRect(i*pw, j*ph, pw, ph);
+};
+
+function clearBlock(i,j){
+  ctx.fillStyle="#ffffff";
+  ctx.fillRect(i*pw, j*ph, pw, ph);
+};
+
+// Logical functions
+
+drawBoard();
+fillBlock(nx/2,ny/2);
+fillBlock(nx/2+1,ny/2+1);
+clearBlock(nx/2+1,ny/2+1);
+clearBlock(nx/2-1,ny/2-1);
